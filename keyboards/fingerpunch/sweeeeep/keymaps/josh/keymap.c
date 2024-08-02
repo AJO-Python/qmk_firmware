1131cab4c3b3973df60d334b777cd3bac9b35acf
AJO-Python <josh.owen121@gmail.com>
2024-08-02 23:32:33 +0100

    Put esc, space, enter on gaming layer left thumbs

--- keyboards/fingerpunch/sweeeeep/keymaps/josh/keymap.c
+++ keyboards/fingerpunch/sweeeeep/keymaps/josh/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,       KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,    KC_U,         KC_I,         TO(_COLEMAK),
     KC_LSFT,      KC_A,         KC_S,         KC_D,         KC_F,         KC_G,         KC_H,    RSFT_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L),
     KC_LCTL,      KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,    KC_M,         KC_COMM,      KC_DOT,
-                                KC_I,        KC_ENT,       KC_QUOT,      KC_BSPC, KC_SPC,       KC_I
+                                KC_ESC,       KC_SPC,       KC_ENT,       KC_SPC,       KC_BSPC, KC_DEL
   )
 };
 
