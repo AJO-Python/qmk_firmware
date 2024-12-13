3a16ef34f6c83efafdda86ad2d99d0df1bfe77fe
AJO-Python <josh.owen121@gmail.com>
2024-12-13 21:49:31 +0000

    sweep-josh: Move symbols on num layer up a row

--- keyboards/fingerpunch/sweeeeep/keymaps/josh/keymap.c
+++ keyboards/fingerpunch/sweeeeep/keymaps/josh/keymap.c
@@ -30,8 +30,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NUMBER] = LAYOUT_split_3x5_3(
-    _______,      _______,          _______,                _______,             _______,                 KC_MINS,               KC_7,                      KC_8,               KC_9,              KC_SCLN,
-    KC_ASTR,      KC_COLON,         KC_MINS,                KC_PLUS,             KC_EQL,                  KC_DOT,                KC_4,                      KC_5,               KC_6,              KC_EQL,
+    KC_ASTR,      KC_COLON,         KC_COMM,                KC_DOT,              KC_MINS,                 KC_MINS,               KC_7,                      KC_8,               KC_9,              KC_SCLN,
+    _______,      _______,          _______,                KC_PLUS,             KC_EQL,                  KC_DOT,                KC_4,                      KC_5,               KC_6,              KC_EQL,
     _______,      KC_SCLN,          KC_UNDERSCORE,          _______,             _______,                 KC_0,                  KC_1,                      KC_2,               KC_3,              KC_KP_PLUS,
                                     _______,                LT(_NUMBER, KC_SPC), _______,                 _______,               _______,                   _______
   ),
