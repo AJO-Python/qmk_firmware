a1fd675d718d4bcae919684078cd1e4c3257e065
AJO-Python <josh.owen121@gmail.com>
2025-02-11 00:32:20 +0000

    matric there but pin assignments not

--- keyboards/bicky_board/config.h
+++ keyboards/bicky_board/config.h
@@ -1,14 +1,4 @@
 #pragma once
 
-/* Split Keyboard Options */
-
-/* Matrix size */
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 10
-
-
-#define MATRIX_ROW_PINS { D13, D2, D3, D4, D28, D27, D26, D22 }
-#define MATRIX_COL_PINS { D9, D8, D7, D6, D5, D20, D23, D21, D16, D14 }
-
-#define DEBOUNCE 5
-#define DIODE_DIRECTION COL2ROW
